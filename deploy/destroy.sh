6d7c0396fc2ea5211db5ec4a0321cdcb434a3a1b
Hyp <hypnguyen1209@gmail.com>
2022-02-06 16:53:22 +0700

    fix bash env

--- deploy/destroy.sh
+++ deploy/destroy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HOST_NAME="node-challenge"
+NAME_CHALL="node-challenge"
 
 MAPPORT=$1
 
-docker rm -f "ctf-${HOST_NAME}-${MAPPORT}"
+docker rm -f "ctf-${NAME_CHALL}-${MAPPORT}"
--- deploy/run_container.sh
+++ deploy/run_container.sh
@@ -2,7 +2,7 @@
 
 MAPPORT=$1
 
-HOST_NAME="node-challenge"
+NAME_CHALL="node-challenge"
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -12,7 +12,7 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-docker run -d --restart=unless-stopped --name "ctf-${HOST_NAME}-${MAPPORT}" \
+docker run -d --restart=unless-stopped --name "ctf-${NAME_CHALL}-${MAPPORT}" \
     -v "${DIR}/src:/opt:ro" \
     -p $MAPPORT:8000 \
-    "ctf-${HOSTNAME}:latest"
+    "ctf-${NAME_CHALL}:latest"
